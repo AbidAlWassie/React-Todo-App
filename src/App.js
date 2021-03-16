4cc4def2a45288fe4c351c1513324b8f0f629776
Abid Al-Wasi <abidalwasi@outlook.com>
2021-03-17 03:26:22 +0600

    updated project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './App.css';
+import Form from "./components/Form";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <header>
         <p>My Todolist</p>
       </header>
+      <Form />
     </div>
   );
 }
