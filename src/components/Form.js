0250dff35239094ef54c87fdd5eab55c89a46b32
Abid Al-Wasi <abidalwasi@outlook.com>
2021-03-17 03:28:22 +0600

    updated project

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,11 +3,11 @@ import React from "react";
 const Form = () => {
   return (
     <form>
-      <input type="text" className={}/>
-      <button type={submit}>
-        <i className={fas fa-plus-square}></i>
+      <input type="text" className={""}/>
+      <button type={"submit"}>
+        <i className="fas fa-plus-square"></i>
       </button>
-      <div className={select}>
+      <div className={"select"}>
         <select name="todos" className="filter-todo">
           <option value="all">all</option>
           <option value="completed">completed</option>
@@ -17,3 +17,5 @@ const Form = () => {
     </form>
   );
 }
+
+export default Form;
